884d8598a0c3d770c417abfa337996eb93b51abb
balagangadhar3676 <gangadhar@geekbull.in>
2020-11-10 11:52:14 +0530

    Addition future add into P0 source code

---
+++ test_p0_gw/lz_p0_gw.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+void add_fun(void)
+{
+    int result;
+    printf("Addition Operation\n");
+    result=5+6;
+    printf("Total addition value is:%d\n",result);
+}
+    
+main()
+{
+    printf("Main Lizard Gateway Source Code\n");
+    printf("Collect the data from uart\n");
+    add_fun();
+}
+
