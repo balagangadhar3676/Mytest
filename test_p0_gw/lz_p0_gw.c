19e2eb524ba26e1b2e471f5701124d853d0b6b0b
mallesh0403 <mallesh@geekbull.in>
2020-11-10 15:41:57 +0530

    updated

--- test_p0_gw/lz_p0_gw.c
+++ test_p0_gw/lz_p0_gw.c
@@ -7,11 +7,20 @@ void add_fun(void)
     result=5+6;
     printf("Total addition value is:%d\n",result);
 }
+
+void diff_fun(void)
+{
+    int diff;
+    printf("subtraction Operation\n");
+    diff=6-5;
+    printf("Total diff value is:%d\n",diff);
+}
     
 main()
 {
     printf("Main Lizard Gateway Source Code\n");
     printf("Collect the data from uart\n");
     add_fun();
+	diff_fun();
 }
 
